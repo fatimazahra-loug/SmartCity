1da2d606022179722341ff5e7e3f0f14c2f44a7c
fatimazahra-loug <fatimazahralougmiri@gmail.com>
2024-10-23 16:08:26 +0100

    adding in controllers and db

--- src/main/java/tuto1/DBusers.java
+++ src/main/java/tuto1/DBusers.java
@@ -1,4 +1,10 @@
 package tuto1;
 
+import javafx.event.ActionEvent;
+
 public class DBusers {
+    public static void signup(ActionEvent event,String username, String password, String confirm){
+
+
+    }
 }
--- src/main/java/tuto1/SignUpController.java
+++ src/main/java/tuto1/SignUpController.java
@@ -1,4 +1,34 @@
 package tuto1;
 
-public class SignUpController {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SignUpController implements Initializable {
+
+    @FXML
+    private Button signup_id;
+    @FXML
+    private TextField username_id;
+    @FXML
+    private PasswordField password_id;
+    @FXML
+    private PasswordField confirm_id;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        signup_id.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+
+            }
+        });
+    }
 }
